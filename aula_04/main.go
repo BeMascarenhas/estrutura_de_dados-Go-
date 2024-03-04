b3683a89f5e9b5530f52f5ccb7be40d9722e7b4e
Bernardo <bemascarenhas6@gmail.com>
2024-03-04 11:32:30 -0300

    aula04

---
+++ aula_04/main.go
@@ -0,0 +1,38 @@
+package main
+//Calculo de Passos
+import(
+	"fmt"
+)
+func buscaMatriz(m[][]int,n int, x int) bool{
+	var i int = 0
+
+	for i<n{
+		j := 0
+		for j <n{
+			if (m[i][j] == x){
+				return true
+			}
+			j++
+		}
+		i++
+	}
+		return false
+}
+func par(a []int, x int) []int {
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i]+a[j] == x {
+				return []int{a[i], a[j]}
+			}
+		}
+	}
+	return []int{-1, -1}
+}
+
+func main(){
+	matriz := [][]int{{1, 2}, {3, 4}}
+	b := buscaMatriz(matriz, 2, 2)
+	fmt.Println(b)
+	c:= []int{1,2,3,4}
+	fmt.Println(par(c,7))
+}
